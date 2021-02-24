3c61f1d3419b45fc65d5da179010dfeedab12db8
Muathye <muath.ye@gmail.com>
2021-02-24 07:33:38 +0300

    Add timestamp()

--- src/helpers.php
+++ src/helpers.php
@@ -2,12 +2,26 @@
 
 use Carbon\Carbon;
 
-/**
- * Returns the Carbon instance
- *
- * @return Carbon
- */
-function carbon() : Carbon
-{
-    return new Carbon;
+if (! function_exists('carbon')) {
+    /**
+     * Returns the Carbon instance
+     *
+     * @return Carbon
+     */
+    function carbon() : Carbon
+    {
+        return new Carbon;
+    }
+}
+
+if (! function_exists('timestamp')) {
+    /**
+     * Get current time stamp
+     *
+     * @return integer
+     */
+    function timestamp()
+    {
+        return carbon()->now()->timestamp;
+    }
 }
